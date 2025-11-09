4d5d33c4f67a5272dab939e0abac9601a1ae6100
Brandon Feist <brandon.feist@hey.com>
2025-11-08 21:35:24 -0800

    reset defending players garden card health at end phase

--- src/systems/end_phase.c
+++ src/systems/end_phase.c
@@ -8,21 +8,30 @@ void HandleEndPhase(ecs_iter_t *it) {
   ecs_world_t *world = it->world;
   GameState *gs = ecs_field(it, GameState, 0);
 
-  ecs_entity_t garden_zone = gs->zones[gs->active_player_index].garden;
-  ecs_entities_t garden_cards = ecs_get_ordered_children(world, garden_zone);
+  ecs_entity_t active_player_garden_zone = gs->zones[gs->active_player_index].garden;
+  ecs_entities_t active_player_garden_cards = ecs_get_ordered_children(world, active_player_garden_zone);
 
-  // TODO: Do i need to reset health of defender cards as well?
-  for (int32_t i = 0; i < garden_cards.count; i++) {
-    cli_render_logf("[EndPhase] Resetting entity health for garden card %d", i);
-    ecs_entity_t garden_card = garden_cards.ids[i];
+  cli_render_logf("[EndPhase] Resetting entity health for active player's garden cards");
+  for (int32_t i = 0; i < active_player_garden_cards.count; i++) {
+    ecs_entity_t garden_card = active_player_garden_cards.ids[i];
 
     reset_entity_health(world, garden_card);
-    // Expire end of turn effects on player's entities
+    // TODO: Expire end of turn effects on player's entities
     discard_equipped_weapon_cards(world, garden_card);
   }
 
+  ecs_entity_t defending_player_garden_zone = gs->zones[(gs->active_player_index + 1) % MAX_PLAYERS_PER_MATCH].garden;
+  ecs_entities_t defending_player_garden_cards = ecs_get_ordered_children(world, defending_player_garden_zone);
+
+  cli_render_logf("[EndPhase] Resetting entity health for defending player's garden cards");
+  for (int32_t i = 0; i < defending_player_garden_cards.count; i++) {
+    ecs_entity_t garden_card = defending_player_garden_cards.ids[i];
+
+    reset_entity_health(world, garden_card);
+  }
+
   ecs_entity_t leader_card = find_leader_card_in_zone(world, gs->zones[gs->active_player_index].leader);
-  // Expire end of turn effects on player's leader
+  // TODO: Expire end of turn effects on player's leader
   discard_equipped_weapon_cards(world, leader_card);
 
   gs->phase = PHASE_START_OF_TURN;
